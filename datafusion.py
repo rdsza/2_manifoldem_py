617db9841cce8ca0b96c0dc0045bfd18b833539a
Raison Dsouza <17195815+rdsza@users.noreply.github.com>
2023-01-09 06:56:49 -0600

    Create datafusion.py

---
+++ datafusion.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn.manifold import SpectralEmbedding
+
+# Load the two datasets
+X1 = np.load("dataset1.npy")
+X2 = np.load("dataset2.npy")
+
+# Concatenate the two datasets
+X = np.concatenate((X1, X2), axis=0)
+
+# Perform spectral embedding on the combined dataset
+se = SpectralEmbedding(n_components=2)
+X_fused = se.fit_transform(X)
+
+# Split the fused dataset back into the original datasets
+X1_fused = X_fused[:len(X1)]
+X2_fused = X_fused[len(X1):]
