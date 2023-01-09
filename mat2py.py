cd5af796c00e9e131e9fd19c85024a33d377696a
rdsza <17195815+rdsza@users.noreply.github.com>
2023-01-09 11:29:03 -0600

    Updated

--- mat2py.py
+++ mat2py.py
@@ -1,5 +1,3 @@
-
-
 import scipy.io
 import numpy as np
 
@@ -22,3 +20,17 @@ def mat_to_npz(mat_file, npz_file):
     np.savez(npz_file, **data)
 
 
+def mat_to_npz_var(mat_file, npz_file, *variables):
+    """
+    You can also specify the variables you want to include in the .npz file 
+    by passing them as keyword arguments to the numpy.savez function,
+    like this:
+    """
+    # Load the .mat file
+    mat = scipy.io.loadmat(mat_file)
+
+    # Convert the loaded .mat file to a dictionary
+    data = {k:v for k, v in mat.items() if k[0] != '_'}
+
+    # Save the specified variables to a .npz file
+    np.savez(npz_file, *[(var, data[var]) for var in variables])
